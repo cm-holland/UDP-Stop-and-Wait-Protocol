b60e591b0030d7c18e51aa94110035d0fb8f726f
cm-holland <cholla89@gmail.com>
2024-01-28 14:00:16 -0500

    Continue work on constructing datagram socket

--- SAWSender.java
+++ SAWSender.java
@@ -5,13 +5,41 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class SAWSender {
+   public  static int BUFFER_SIZE = 1024;
     public static int RECEIVER_PORT = 6789;
     public static int SENDER_PORT = 5678;
     public static int HEADER_SIZE = 10;
     public static int WINDOW_SIZE = 1; //Window size of 1 for stop-and-wait protocol
-    private DatagramSocket senderSocket;
-    public SAWSender(int port) throws SocketException{
-        this.senderSocket = new DatagramSocket(port);
+
+    public static void main(String[] args) {
+        //Creating datagram socket
+        try {
+            DatagramSocket senderSocket = new DatagramSocket();
+            InetAddress receiverAddress = InetAddress.getLocalHost();
+
+            //Input stream to read bytes from file
+            File file = new File("COSC635_P2_DataSent.txt");
+            FileInputStream fileInput = new FileInputStream(file);
+            byte [] data = new byte [BUFFER_SIZE];
+            FileInputStream.read(data);
+            FileInputStream.close();
+
+            //
+            int packetLossPercentage = getPacketLossPercentage();
+
+            int sequenceNumber = 0;
+            int totalPacketsSent = 0;
+            int totalPacketsLost = 0;
+
+
+
+
+
+
+
+
+        }
+
     }
 
 
